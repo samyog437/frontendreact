e720045b61a2d7e9e9f3b545c69be5af389cb925
Samyog Bohara <fallendemon174@gmail.com>
2023-01-09 08:33:13 +0545

    forms

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 // import { useState } from 'react';
 import './App.css';
 
-// import Feedback_app  from './components/Feedback_app';
-import History_app from './components/History_app';
-
-
-function App({name,age}) {
-
-  
-  // const [counter,setCounter]= useState(0)
-
-  // const handleCounter = () =>{
-  //   setCounter(counter+1)
-  // }
-
+function App(props) {
 
   return (
     <>
-   
-   {/* <h2>{counter}</h2>
-   <button onClick={handleCounter}>Plus</button> */}
-   {/* <Feedback_app/> */}
-   <History_app/>
-
+    <h2>Notes</h2>
     </>
-    
-
   );
 }
 
